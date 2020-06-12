8218867ff93906b55d724cb3197d3803e5714fb0
tracy <843358687@qq.com>
2020-06-12 13:44:56 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>当前路径不存在，请检查网站是否正确</div>
+    <div>
+      <router-link to="/money">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+</style>
