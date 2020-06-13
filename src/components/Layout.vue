bbddaeb02ec4f5776cd8db20d16f2094419bc94c
tracy <843358687@qq.com>
2020-06-13 10:15:25 +0800

    添加Nav样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -8,13 +8,11 @@
 </template>
 <style lang='scss' scoped>
 .nav-wrapper {
-  border: 1px solid green;
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .content {
-  border: 1px soli blue;
   overflow: auto;
   flex-grow: 1;
 }
