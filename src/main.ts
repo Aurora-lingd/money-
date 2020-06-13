95363064495228ea95374419493ba5b98bad3143
tracy <843358687@qq.com>
2020-06-13 17:11:47 +0800

    添加全局样式和全局变量

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
 // js有个潜规则如果它引入了目录，那么它会直接找index
-import router from './router'
-import store from './store'
-import Layout from '@/components/Layout.vue'
-import Icon from '@/components/Icon.vue'
-import Nav from '@/components/Nav.vue'
-Vue.config.productionTip = false
+import router from './router';
+import store from './store';
+import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
+import Nav from '@/components/Nav.vue';
 
-Vue.component('Nav', Nav)
-Vue.component('Layout', Layout)
-Vue.component('Icon', Icon)
+Vue.config.productionTip = false;
+
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
