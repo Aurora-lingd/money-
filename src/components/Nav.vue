855294df624f041ee46efd831b874384ce7a765f
tracy <843358687@qq.com>
2020-06-15 15:46:01 +0800

    Money HTML和CSS写好了

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,51 +1,48 @@
 <template>
-    <nav>
-        <router-link to="/money" class="item" active-class="selected">
-            <Icon name="money"/>
-            记账
-        </router-link>
-        <router-link to="/labels" class="item" active-class="selected">
-            <Icon name="lable"/>
-            标签
-        </router-link>
-        <router-link to="/statistics" class="item" active-class="selected">
-            <Icon name="statistics"/>
-            明细
-        </router-link>
-    </nav>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money" />记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="lable" />标签
+    </router-link>
+    <router-link to="/statistics" class="item" active-class="selected">
+      <Icon name="statistics" />明细
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'Vue'
-    };
+export default {
+  name: "Vue"
+};
 </script>
 
 <style lang='scss' scoped>
-    @import "~@/assets/style/helper.scss";
+@import "~@/assets/style/helper.scss";
 
-    nav {
-        display: flex;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-        flex-direction: row;
-        font-size: 12px;
+nav {
+  @extend %outerShadow;
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
 
-        > .item {
-            padding: 2px 0;
-            width: 33.3333%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
+  > .item {
+    padding: 2px 0;
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
-            .icon {
-                width: 32px;
-                height: 32px;
-            }
-        }
-
-        > .item.selected {
-            color: $color-highlight;
-        }
+    .icon {
+      width: 32px;
+      height: 32px;
     }
+  }
+
+  > .item.selected {
+    color: $color-highlight;
+  }
+}
 </style>
