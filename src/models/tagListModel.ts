05aefcd4e351ea6d13f36c82fd2b5c1be269bcfb
tracy <843358687@qq.com>
2020-06-20 17:58:56 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -8,22 +8,54 @@ type TagListModel = {
  initDate: string;
  fetch: () => Tag[];
  create: (name: string) => 'success' | 'duplicated';
+ update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+ remove: (id: string) => boolean;
  save: () => void;
 }
 
 const tagListModel: TagListModel = {
- data:[],
- initDate:'["衣","食","住","行"]',
+ data: [],
+ initDate: '["衣","食","住","行"]',
  fetch() {
   this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
-  return this.data
+  return this.data;
  },
- create(name){
-  const names = this.data.map(item => item.name)
-  if(names.indexOf(name)>=0){return 'duplicated'}
-  this.data.push({id:name,name:name})
+ create(name) {
+  const names = this.data.map(item => item.name);
+  if (names.indexOf(name) >= 0) {
+   return 'duplicated';
+  }
+  this.data.push({id: name, name: name});
+  this.save();
+  return 'success';
+ },
+ update(id, name) {
+  const idList = this.data.map(item => item.id);
+  if (idList.indexOf(id) >= 0) {
+   const names = this.data.map(item => item.name);
+   if (names.indexOf(name) >= 0) {
+    return 'duplicated';
+   } else {
+    const tag = this.data.filter(item => item.id === id)[0];
+    tag.name = name;
+    this.save();
+    return 'success';
+   }
+  } else {
+   return 'not found';
+  }
+ },
+ remove(id: string) {
+  let index = -1;
+  for (let i = 0; i < this.data.length; i++) {
+   if (this.data[i].id === id) {
+    index = i;
+    break;
+   }
+  }
+  this.data.splice(index, 1);
   this.save()
-  return 'success'
+  return true;
  },
  save() {
   window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,18 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-Wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="updateTag"
+                field-name="标签名"
+                placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -20,6 +23,7 @@
  import tagListModel from '@/models/tagListModel';
  import FormItem from '@/components/Money/FormItem.vue';
  import Button from '@/components/Button.vue';
+
  type Tag = {
   id: string;
   name: string;
@@ -29,7 +33,7 @@
  })
 
  export default class EditLabel extends Vue {
-  tag?: Tag = undefined
+  tag?: Tag = undefined;
 
   created() {
    const id = this.$route.params.id;
@@ -41,6 +45,19 @@
    } else {
     this.$router.replace('/404');
    }
+  }
+  updateTag(name: string){
+   if(this.tag){
+    tagListModel.update(this.tag.id,name)
+   }
+  }
+  remove(){
+   if(this.tag){
+    tagListModel.remove(this.tag.id)
+   }
+  }
+  goBack(){
+   this.$router.back()
   }
  }
 </script>
