ef9fdc1d532f57056df7cd57a99090f6e6eb55f1
tracy <843358687@qq.com>
2020-06-22 14:40:21 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,19 +17,15 @@
  import Types from '@/components/Money/Types.vue';
  import FormItem from '@/components/Money/FormItem.vue';
  import Tags from '@/components/Money/Tags.vue';
- import {Component, Watch} from 'vue-property-decorator';
- import recordListModel from '@/models/recordListModel';
+ import {Component} from 'vue-property-decorator';
 
-
-
- const recordList = recordListModel.fetch();
 
  @Component({
   components: {NumberPad, Types, FormItem, Tags}
  })
  export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
    tags: '衣', notes: '', type: '-', amount: 0
   };
@@ -43,14 +39,8 @@
   }
 
   saveRecord() {
-   recordListModel.create(this.record)
+   window.createRecord(this.record)
   }
-
-  @Watch('recordList')
-  onRecordListChange() {
-   recordListModel.save();
-  }
-
 
  }
 </script>
