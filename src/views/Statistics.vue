3f0b32184e243812c02c2c44535727867003eaba
tracy <843358687@qq.com>
2020-06-28 10:46:32 +0800

    完成列表的 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,11 +3,19 @@
     <Layout>
       <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
       <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-      <div>
-        type:{{type}}
-        <br/>
-        interval:{{interval}}
-      </div>
+        <ol>
+          <li v-for="(group,index) in result" :key="index">
+            <h3 class="title">{{group.title}}</h3>
+            <ol>
+              <li v-for="item in group.items" :key="item.id" class="record">
+                <span>{{item.tags}}</span>
+                <span class="notes">{{item.notes}}</span>
+                <span>￥{{item.amount}}</span>
+
+              </li>
+            </ol>
+          </li>
+        </ol>
     </Layout>
   </div>
 </template>
@@ -25,8 +33,31 @@
  export default class Statistics extends Vue {
   type = '-';
   interval = 'day';
-  intervalList =intervalList;
+  intervalList = intervalList;
   recordTypeList = recordTypeList;
+
+  get recordList() {
+   return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+   const {recordList} = this;
+   type HashTableValue = { title: string; items: RecordItem[] }
+
+   const hashTable: { [key: string]: HashTableValue } = {};
+   for (let i = 0; i < recordList.length; i++) {
+    const [date, time] = recordList[i].createdAt!.split('T');
+    console.log(date);
+    hashTable[date] = hashTable[date] || {title: date, items: []};
+    hashTable[date].items.push(recordList[i]);
+   }
+   console.log(hashTable);
+   return hashTable;
+  }
+
+  created() {
+   this.$store.commit('fetchRecords');
+  }
  }
 </script>
 
@@ -43,8 +74,28 @@
         }
       }
     }
-    .interval-tabs-item{
-      height:48px
+
+    .interval-tabs-item {
+      height: 48px
     }
+  }
+  %item{
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+  .title{
+    background: #d4d1cd;
+    @extend %item;
+  }
+  .record{
+    @extend %item;
+  }
+  .notes{
+    margin-right:auto ;
+    margin-left: 16px;
+    color: #999;
   }
 </style>
