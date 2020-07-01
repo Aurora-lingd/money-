c89d575573bbfc7f28cdc2a5c4f5528b51633b0f
tracy <843358687@qq.com>
2020-07-01 11:46:28 +0800

    完善 加减 和 OK

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok" @click="ok">{{OK}}</button>
+      <button class="ok" @click="ok">OK</button>
       <button @click="inputContent">0</button>
       <button @click="inputContent">.</button>
       <button @click="clear">清空</button>
@@ -27,56 +27,57 @@
 
  @Component
  export default class NumberPad extends Vue {
-  type = '';
   output = '0';
-  OK = 'OK';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   num: Array<any> = [];
-  count1 = 0;
-  count2 = 0;
+  count = 0;
 
   inputContent(event: MouseEvent) {
    const button = (event.target as HTMLButtonElement);
    const input = button.textContent as string;
-   if (input === '+' || input === '-') {
-    // if (this.output.indexOf('+') >= 0) {
-    //  this.count1 = 0;
-    // }
-    // if (this.output.indexOf('-') >= 0) {
-    //  this.count1 = 0;
-    // }
-    if (this.num.length === 0) {
-     this.num.push(this.output);
+
+    if ((this.output.indexOf('+') >= 0 || this.output.indexOf('-') >= 0) && input !== '-' && input !== '+') {
+     this.output = input;
+     this.count = 1
+     return;
+    }
+    if (this.output.indexOf('+') >= 0 && input === '-' ){
+     return;
+    }else if (this.output.indexOf('-') >= 0 && input === '+' ) {
+     return;
+    } else if (this.output.indexOf('+') >= 0 && input=== '+' ){
+     console.log(this.num)
+     this.count = 1
+     return;
+    }else if (this.output.indexOf('-') >= 0 && input=== '-' ) {
+     console.log(this.num)
+     this.count = 1
+     return;
     } else {
+    if (this.output.indexOf('+') >= 0 || this.output.indexOf('-') >= 0) {
+     this.output = input;
+     this.count = 1
+     return;
+    }
+    if (input === '+' || input === '-') {
      this.num.push(this.output);
     }
-   } else if (this.output.indexOf('+') >= 0 || this.output.indexOf('-') >= 0) {
-    this.output = input;
-    return;
-   }
 
-   if (this.output.indexOf('+') === -1 && input === '+') {
-    if (this.count1 <= 1){
-     this.num.push('+')
+    if (this.output.indexOf('+') === -1 && input === '+') {
+     if (this.count <= 1){
+      this.num.push('+')
+     }
+     this.count += 1
     }
-    this.count1 += 1
-   }
-   if (this.output.slice(1).indexOf('-') === -1 && input === '-') {
-    if (this.count1 <= 1){
-     this.num.push('-')
+    if (this.output.slice(1).indexOf('-') === -1 && input === '-') {
+     if (this.count <= 1){
+      this.num.push('-')
+     }
+     this.count += 1
     }
-    this.count1 += 1
    }
-   console.log(this.count1)
 
-
-   if (this.num.indexOf('+') && input === '-' ){
-    this.num[1] = '-'
-    //this.num = this.num.slice(0,2)
-    console.log(this.num)
-    this.count1 = 1
-   }
-   if (this.count1 >= 2) {
+   if (this.count >= 2) {
     let result;
     let n
     if (this.num.slice(0,3).indexOf('+') >= 0) {
@@ -91,7 +92,7 @@
     }
     this.num.unshift(result);
     this.output = this.num[0];
-    this.count1 = 1;
+    this.count = 1;
    }
 
    if (this.output.length === 16) {
@@ -108,58 +109,40 @@
    if (this.output.indexOf('.') >= 0 && input === '.') {
     return;
    }
-   if (this.output.indexOf('+') >= 0 && input === '+') {
-    this.num.pop();
-    return;
-   }
-   if (this.output[0] == '-') {
-    if (this.output.slice(1).indexOf('-') >= 0 && input === '-') {
-     this.num.pop();
-     return;
-    }
-   } else if (this.output.indexOf('-') >= 0 && input === '-') {
-    this.num.pop();
-    return;
-   }
-
-   // if (this.num.indexOf('+') >= 0) {
-   //  if (input === '-') {
-   //   console.log('fuck-----------------');
-   //   this.output = this.output.replace('+', '');
-   //   this.num.pop();
-   //
-   //  }
-   // }
-   if (this.output[0] !== '-' && this.output.indexOf('-') >= 0) {
-    if (input === '+') {
-     this.output = this.output.replace('-', '');
-     this.num.pop();
-    }
-   }
-   if (this.output[0] == '-' && this.output.indexOf('-') >= 0) {
-    if (input === '+') {
-     this.output = '-' + this.output.slice(1).replace('-', '');
-     this.num.pop();
-    }
-   }
-   console.log(this.num);
-
-
    this.output += input;
 
   }
-
   clear() {
    this.output = '0';
    this.num.length = 0;
-   this.count1 = 0;
+   this.count = 0;
   }
 
   ok() {
-   if (this.OK === 'OK') {
+   console.log(this.num.length);
+   console.log(this.num);
+
+   if (this.output.indexOf('+') >=0 || this.output.slice(1).indexOf('-') >= 0){
     const number = parseFloat(this.output);
     this.$emit('update:value', number);
     this.$emit('submit', number);
+    this.output = '0';
+   }else {
+    this.num.push(this.output);
+    let result;
+    let n
+    if (this.num.slice(0,3).indexOf('+') >= 0) {
+     n = this.num.slice(0,3).join('').split('+').map(i => parseFloat(i))
+     this.num = this.num.slice(3)
+     result =n[0] + n[1]
+    }
+    if (this.num.slice(1,3).indexOf('-') >= 0) {
+     n = this.num.slice(0,3).join('').split('-').map(i => parseFloat(i))
+     this.num = this.num.slice(3)
+     result = n[0] - n[1]
+    }
+    this.$emit('update:value', result);
+    this.$emit('submit', result);
     this.output = '0';
    }
   }
